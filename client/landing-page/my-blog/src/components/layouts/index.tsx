8d595f237863e60c0e475f0ec8b030c8921d5174
locDEV169 <loc16910@gmail.com>
2022-05-31 16:18:18 +0700

    UI login + upgrade react-route-dom + add antd + react-gg-login

--- client/landing-page/my-blog/src/components/layouts/index.tsx
+++ client/landing-page/my-blog/src/components/layouts/index.tsx
@@ -1,14 +1,15 @@
-import { default as React, ReactNode } from "react";
+import { default as React } from "react";
 import HeaderLayout from "./header";
 import FooterLayout from "./footer/index";
+import { Outlet } from "react-router-dom";
 
-export function MainLayout({ children }: { children: ReactNode }) {
+export function MainLayout() {
     return (
         <div id="__next">
             <HeaderLayout />
             <main className="layout-content">
                 <div className="wrap-content">
-                    {children}
+                    <Outlet></Outlet>
                 </div>
             </main>
             <FooterLayout />

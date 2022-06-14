28fbe7ac037f9a351dcf14f590ebee81dd17c86d
Toilahuy <duonghuy5199@gmail.com>
2022-06-14 14:45:50 +0700

    done register page by Huy

--- client/landing-page/my-blog/src/pages/register-page/index.tsx
+++ client/landing-page/my-blog/src/pages/register-page/index.tsx
@@ -3,101 +3,133 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/role-supports-aria-props */
 /* eslint-disable jsx-a11y/alt-text */
-import { default as Button } from 'antd/es/button';
-import 'antd/es/button/style/index.css';
-import { default as Form } from 'antd/es/form';
-import 'antd/es/form/style/index.css';
-import { default as Input } from 'antd/es/input';
-import 'antd/es/input/style/index.css';
-import LoginGooglePage from 'components/auth/login.component';
+import { default as Button } from "antd/es/button";
+import "antd/es/button/style/index.css";
+import { default as Form } from "antd/es/form";
+import "antd/es/form/style/index.css";
+import { default as Input } from "antd/es/input";
+import "antd/es/input/style/index.css";
+import LoginGooglePage from "components/auth/login.component";
+import api from "constants/api";
 import React from "react";
+import { default as message } from "antd/es/message";
 
 interface User {
-    email?: string
-    password?: string
+  email?: string;
+  password?: string;
+}
+interface ErrorType {
+  response: {
+    status?: number;
+    data: {
+      msg: string;
+      message?: string;
+    };
+  };
 }
 
 export default function RegisterPage() {
+  const onFinish = (values: User) => {
+    api
+      .post("register", { account: values.email, password: values.password })
+      .then((res) => {
+        message.success("Register Successful");
+        window.location.href = "/login";
+      })
+      .catch((errors: ErrorType) => handlerError(errors));
+  };
+  const handlerError = (err: ErrorType) => {
+    const status = err.response?.status;
+    switch (status) {
+      case 400:
+        message.error(err.response.data.msg);
+        break;
+      case 401:
+        message.error(err.response.data.message);
+        break;
+      case 500:
+        message.error(err.response.data.message);
+        break;
+      default:
+        message.error("Request Login Failed");
+    }
+  };
 
-    const onFinish = (values: User) => {
-       console.log(values)
-    };
-
-    return (
-        <div id="__next">
-            <div>
-                <div className="form">
-                    <div className="form-header">
-                        <a href="/">
-                            <img src="https://vietcetera.com/images/header/logo-text.svg" alt="Vietcetera" width="100%" height="auto" />
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div className="form-account login-page">
-                <h1 style={{ fontWeight: '100', marginBottom: '12px' }}>Đăng Ký</h1>
-                <p className="text-body-2-light">Nhập email của bạn để đăng nhập tài khoản.</p>
-                <div className="account-grid mb-input">
-                    <Form
-                        name="normal_login"
-                        className="login-form .jss62"
-                        initialValues={{
-                            remember: true,
-                        }}
-                        onFinish={onFinish}
-                    >
-                        <Form.Item
-                            name="email"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your Username!',
-                                },
-                            ]}
-                        >
-                            <Input placeholder="Email" />
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your Password!',
-                                },
-                            ]}
-
-                        >
-                            <Input.Password
-                                type="password"
-                                placeholder="Password"
-                            />
-                        </Form.Item>
-
-                        <Form.Item className='login-button-form'>
-                            <a href="">Quên Mật Khẩu</a>
-                            <Button type="primary" htmlType="submit" className="login-form-button">
-                                Đăng Nhập
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </div>
-                {/*  */}
-                <div className=''>
-                    <LoginGooglePage />
-                </div>
-                {/* Link đăng ký */}
-                <div className="form-span author justify-content-center">
-                    <div>
-                        <span className="text-body-2">
-                            Bạn chưa có tài khoản?
-                        </span>
-                        <span className="secondary left">
-                            Đăng ký
-                        </span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div id="__next">
+      <div>
+        <div className="form">
+          <div className="form-header">
+            <a href="/">
+              <img
+                src="https://vietcetera.com/images/header/logo-text.svg"
+                alt="Vietcetera"
+                width="100%"
+                height="auto"
+              />
+            </a>
+          </div>
         </div>
-    );
-}
+      </div>
+      <div className="form-account login-page">
+        <h1 style={{ fontWeight: "100", marginBottom: "12px" }}>Đăng Ký</h1>
+        <p className="text-body-2-light">Nhập email của bạn để ký tài khoản.</p>
+        <div className="account-grid mb-input">
+          <Form
+            name="normal_login"
+            className="login-form .jss62"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Username!",
+                },
+              ]}
+            >
+              <Input placeholder="Email" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Password!",
+                },
+              ]}
+            >
+              <Input.Password type="password" placeholder="Password" />
+            </Form.Item>
 
+            <Form.Item className="login-button-form">
+              <a href="">Quên Mật Khẩu</a>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Đăng Nhập
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+        {/*  */}
+        <div className="">
+          <LoginGooglePage />
+        </div>
+        {/* Link đăng ký */}
+        <div className="form-span author justify-content-center">
+          <div>
+            <span className="text-body-2">Bạn chưa có tài khoản?</span>
+            <span className="secondary left">Đăng ký</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/landing-page/my-blog/src/routes/index.tsx
+++ client/landing-page/my-blog/src/routes/index.tsx
@@ -1,23 +1,22 @@
 import { MainLayout } from "components/layouts";
 import LoginPage from "pages/login";
+import RegisterPage from "pages/register-page";
 import { default as React, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/home";
-import RegisterPage from '../pages/register-page/index';
 // const HomePage = React.lazy(() => import('../pages/home'))
 // const LoginPage = React.lazy(() => import('../pages/login'))
 
-
 export function MainRoutes() {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                <Route path="/" element={<MainLayout />}>
-                    <Route path="/" element={<HomePage />} />
-                </Route>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-            </Routes>
-        </Suspense>
-    );
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Suspense>
+  );
 }
